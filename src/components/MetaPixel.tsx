74eddb25bd63951b25124a27f1eaf89de1daf061
Onkar Vaidya <omkarvaidya166@gmail.com>
2026-08-20 00:42:47 +0530

    Analytics: Use raw Meta Pixel script injection for extension detection

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -1,30 +1,46 @@
 "use client";
 
-import { useEffect } from "react";
-import { initPixel, trackPageView } from "@/lib/pixel";
-import { captureAttribution } from "@/lib/attribution";
+import Script from "next/script";
 import { siteConfig } from "@/lib/config";
+import { useEffect } from "react";
+import { captureAttribution } from "@/lib/attribution";
 
 export function MetaPixel() {
   useEffect(() => {
-    if (siteConfig.pixelId) {
-      initPixel(siteConfig.pixelId);
-      trackPageView();
-    }
     captureAttribution();
   }, []);
 
   if (!siteConfig.pixelId) return null;
 
   return (
-    <noscript>
-      <img
-        height="1"
-        width="1"
-        style={{ display: "none" }}
-        src={`https://www.facebook.com/tr?id=${siteConfig.pixelId}&ev=PageView&noscript=1`}
-        alt=""
+    <>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${siteConfig.pixelId}');
+            fbq('track', 'PageView');
+          `,
+        }}
       />
-    </noscript>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${siteConfig.pixelId}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
   );
 }
